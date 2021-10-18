bac022b036798a4326178035cd18e73573b0d349
Duncan Herington <87780351+Dherington1@users.noreply.github.com>
2021-10-17 22:47:53 -0700

    added delete feature

--- Routes/apiroutes.js
+++ Routes/apiroutes.js
@@ -4,31 +4,60 @@ const path = require("path");
 const { v4: uuidv4 } = require('uuid');
 
 router.post("/notes", (req, res) => {
-  const notes = JSON.parse(
-    fs.readFileSync(path.join(__dirname, "../db/db.json"), "utf-8")
-  );
+  // reads file from our db folder file
+  const notes = JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json"), "utf-8"));
 
+  // connects to the body 
   const body = req.body;
+  // creates a random number for our note (like ID)
   body.id = uuidv4();
+
+  // pushes our note to the posted notes part of page (left side)
   notes.push(body);
 
-  fs.writeFileSync(
-    path.join(__dirname, "../db/db.json"), JSON.stringify(notes), "utf-8"
-  );
+  // writes our notes to our db file as a string
+  fs.writeFileSync(path.join(__dirname, "../db/db.json"), JSON.stringify(notes), "utf-8");
 
+  // sends data to the page as json
   res.json(body);
 });
 
 // get our posted notes
 router.get("/notes", (req, res) => {
   // reads file from our db folder file
-  const notes = JSON.parse(
-    fs.readFileSync(path.join(__dirname, "../db/db.json"), "utf-8")
-  );
+  const notes = JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json"), "utf-8"));
 
   // sends data to page as json
   res.json(notes);
 });
+
+
+router.delete('/notes/:id', (req, res) => {
+
+  // const notes = JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json"), "utf-8"));
+  
+  // read db.json, filter and write db.json
+  const { id } = req.params;
+  let doNotDel;
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+      const oldData = JSON.parse(data);
+      // oldData = oldData.filter(note => note.id !== req.params.id);
+      if (err) {
+          res.status(500).send(err);
+      }
+      doNotDel = oldData.filter((note) => {
+          return note.id !== id;
+      });
+      console.log(oldData);
+      fs.writeFile("./db/db.json", JSON.stringify(doNotDel), (err) => {
+          if (err) {
+              res.status(500).send(err);
+          }
+      });
+      res.send(doNotDel)
+      // res.json("Success!");
+  });  
+})
 
 module.exports = router;
 
